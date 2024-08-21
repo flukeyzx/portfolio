69fdf36c57d3166498efbd9f8e0cf3e72fbb5dbd
unknown <ahadpubgmgamer@gmail.com>
2024-08-21 01:11:16 -0700

    bug fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,11 @@ const Header = () => {
     <nav className="py-6 px-8">
       <div className="flex justify-between items-center">
         <h2 className="text-2xl font-semibold flex items-center gap-2 cursor-pointer">
-          <span className="gradient-letter text-3xl max-sm:text-lg">
+          <span className="gradient-letter text-3xl max-sm:hidden">
             {"<Portfolio/>  "}
           </span>
         </h2>
-        <div className="flex items-center gap-8">
+        <div className="flex items-center max-sm:justify-center max-sm:w-full gap-8">
           <ThemeController />
           <div className="flex gap-2 text-lg cursor-pointer max-sm:hidden">
             <a href="#skills" className="hover:text-primary">
