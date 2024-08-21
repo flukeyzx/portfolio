b1ee1331b3fe38e1de50026b8c9d0487747376e4
unknown <ahadpubgmgamer@gmail.com>
2024-08-21 00:44:12 -0700

    add loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,9 @@
 const Loader = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <span className="loading loading-spinner text-primary loading-lg"></span>
+    </div>
+  );
 };
 
 export default Loader;
